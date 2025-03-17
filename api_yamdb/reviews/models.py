776143da9a89cad5ec7648afd98e4a877fdd49bd
Dzmitry Radziuk <mitia.radiuk@yandex.ru>
2025-03-17 17:49:20 +0300

    Добавил заглушки в модели. Пока будем использовать встроенную юзер-модель, по мере необходимости кастомизируем. Пушу сразу в ветку develop.

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Titles(models.Model):
+    pass
+
+class Reviews(models.Model):
+    pass
+
+class Categories(models.Model):
+    pass
+
+class Genres(models.Model):
+    pass
+
+class Comments(models.Model):
+    pass
+
+
+
+
